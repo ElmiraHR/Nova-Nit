c7c686a35872884cc62fe8605003ead9cb7390d3
ElmiraHR <elhrachyan@gmail.com>
2025-04-26 19:11:17 +0200

    added Login styles

--- adminpanel/src/pages/Login.tsx
+++ adminpanel/src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import styles from './Login.module.css'
 
 interface LoginResponse {
   status: 'success' | 'error';
@@ -47,25 +48,28 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h2>Login to Admin Panel</h2>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <form onSubmit={handleLogin}>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        /><br />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        /><br />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <div className={styles.container}>
+  <h2 className={styles.title}>Login to Admin Panel</h2>
+  {error && <p className={styles.error}>{error}</p>}
+  <form onSubmit={handleLogin} className={styles.form}>
+    <input
+      type="text"
+      placeholder="Username"
+      value={username}
+      onChange={(e) => setUsername(e.target.value)}
+      className={styles.input}
+    /><br />
+    <input
+      type="password"
+      placeholder="Password"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+      className={styles.input}
+    /><br />
+    <button type="submit" className={styles.button}>Login</button>
+  </form>
+</div>
+
   );
 };
 
